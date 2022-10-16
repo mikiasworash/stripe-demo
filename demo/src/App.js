832779ef220498e6bee4ea327ec111c0a6434ef8
mikias-worash <mikias.worash.a@gmail.com>
2022-10-16 18:03:41 +0300

    added react.js

---
+++ demo/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+
+      <form action="../../post" method="post" className="form">
+        <button type="submit">Connect to Nodejs</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
